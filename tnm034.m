33c3e8d904ab17b7b7e72b9b05b2e0b54d304064
Samuel Larsson <samlarsson94@gmail.com>
2020-11-12 09:47:03 +0100

    test rotation

--- tnm034.m
+++ tnm034.m
@@ -5,16 +5,46 @@
 % i.e. ‘1’, ‘2’,...,‘16’ for the persons belonging to ‘db1’ 
 % and ‘0’ for all other faces.
 function id = tnm034(im)
-    myimg = im2double(imread('images/DB1/db1_02.jpg'));
+    myimg = im2double(imread('images/DB1/db1_04.jpg'));
     myimg = whiteWorldCorrection(myimg);
     myimg = faceMask(myimg);
     
-    eye = eyeMap(myimg) > 0.9;
-    mouth = mouthMap(myimg) > 0.9;
-    mask = eye + mouth;
-    mask(mask > 1) = 1;
+%     figure
+%     imshow(myimg);
     
-    myimg = myimg .* mask;
+    eye = eyeMap(myimg) > 0.8;
     
-    imshow(myimg);
+    figure
+    imshow(eye);
+    
+    x = regionprops(eye, 'centroid');
+    c = cat(1, x.Centroid);
+    
+    xlength = abs(c(1,1) - c(1,2));
+    ylength = abs(c(2,1) - c(2,2));
+    
+    hypo = sqrt(xlength^2 + ylength^2);
+    
+    angle = acosd(xlength / hypo);
+    
+    newimg = imrotate(myimg, -angle);
+    
+    imshow(newimg);
+    
+    %test = atan2d(c(:, 1), c(:, 2));
+    
+    %imshow(eye);
+    hold on;
+    plot(c(:, 1), c(:,2), 'red');
+    
+    %size(c)
+   
+    
+%     mouth = mouthMap(myimg) > 0.9;
+%     mask = eye + mouth;
+%     mask(mask > 1) = 1;
+%     
+%     myimg = myimg .* mask;
+%     
+%     imshow(myimg);
 end       
