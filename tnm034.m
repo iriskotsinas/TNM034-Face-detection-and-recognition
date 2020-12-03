bcb22173f8e87c32cc115cf51c2fe1aa75dc29b6
Samuel Larsson <samla949@student.liu.se>
2020-12-03 14:01:28 +0100

    cleanup

--- tnm034.m
+++ tnm034.m
@@ -4,92 +4,30 @@
 % id: The identity number (integer) of the identified person,
 % i.e. ‘1’, ‘2’,...,‘16’ for the persons belonging to ‘db1’ 
 % and ‘0’ for all other faces.
-function id = tnm034(im)
-    meanFace = getMeanFace(); 
-    [u, w] = getEigenfaces(meanFace);
+function id = tnm034(im)    
+    %[weights, eigenFaces, meanFace] = train();
+    %save('data.mat', 'weights', 'eigenFaces', 'meanFace');
     
-    org = im2double(imread('images/DB1/db1_01.jpg'));
+    load('data.mat', 'weights', 'eigenFaces', 'meanFace');
     
-    img = detection(org);
-    img = rgb2gray(img);
+    % TODO: im ska avändas här egentligen...!
+    uri = 'images/DB1/db1_13.jpg';
     
-    test = reshape(img, [], 1);
-    test = test - meanFace;
-
+    testImg = detection(im2double(imread(uri)));
+    testImg = rgb2gray(testImg);
+    testImg = reshape(testImg, [], 1);
+    testImg = testImg - meanFace;
     
-    ww = u' * test;
-
-    [val, idx] = min(sum(sqrt((ww-w).^2),1));
-    idx
-    %[V,index] = min(sum(sqrt((ww-w).^2)));
-    %index
-
+    testWeights = eigenFaces' * testImg;
     
-%     figure(1);
-%     imshow(img);
-%     
-%     load('eigenfaces.mat');
-%     load('meanface.mat');
-%     
-%     img = reshape(img, [], 1);
-%     
-%     phi = img - meanface;
-%     
-%     
-%     
-%     w = phi' * A;
-%     w = normr(w);
-%     w
+    [magnitude, index] = min(sum((testWeights - weights).^2, 1));
     
+    % TODO: undersök vilket värde på felet
+    if magnitude > 50
+        id = 0;
+    else
+        id = index;
+    end
     
-    
-%     W = zeros(1, 16);
-%     for k = 1:1:16
-%         W(1,i) = eigenfaces * phi;
-%     end
-
-
-%     figure(1);
-%     clf
-%     
-%     meanFace = zeros(301*282, 1);
-%     faces = zeros(301*282, 16);
-%     
-%     for i=1:1:16
-%         if i <= 9
-%             org = im2double(imread(sprintf('images/DB1/db1_0%d.jpg', i)));
-%         else
-%             org = im2double(imread(sprintf('images/DB1/db1_%d.jpg', i))); 
-%         end
-%         
-%         img = detection(org);
-%         img = rgb2gray(img);
-%         
-%         subplot(4, 4, i);
-%         imshow(img);
-%         
-%         
-%         vec = reshape(img, [], 1);
-%         
-%         faces(:, i) = vec;
-%         
-% 
-%         
-%         meanFace = meanFace + vec;
-%         
-%         %imshow(org);
-%         %hold on;
-%         %plot(eyePair(:, 1), eyePair(:, 2), 'r*');
-%     end
-%     
-%     meanFace = meanFace .* (1/16);
-%     
-%     figure(2);
-%     meanFace = reshape(meanFace, [301, 282]);
-%     imshow(meanFace);
-%    
-%        test = eigenface(meanFace, faces);
-
-    
-    
+    clear weights eigenFaces meanFace;
 end
---
+++ train.m
@@ -0,0 +1,56 @@
+function [weights, eigenFaces, meanFace] = train()
+    % cropped image dimensions
+    w = 282;    
+    h = 301;
+    
+    n = 16;
+    
+    meanFace = zeros(w*h, 1);
+    phi = zeros(w*h, 16);
+    
+    
+    for k = 1:1:n
+        if k <= 9
+            img = im2double(imread(sprintf('images/DB1/db1_0%d.jpg', k)));
+        else
+            img = im2double(imread(sprintf('images/DB1/db1_%d.jpg', k))); 
+        end
+        
+        % crop image to face
+        img = detection(img);
+        
+        % color => black/white
+        img = rgb2gray(img);  
+        
+        % reshape to column vector
+        img = reshape(img, [], 1);
+        
+        % append to meanface
+        meanFace = meanFace + img;
+        
+        % add to phi
+        phi(:, k) = img;
+    end
+    
+    meanFace = meanFace ./ n;
+    
+    for k = 1:1:n
+        phi(:, k) = phi(:, k) - meanFace;
+    end
+    
+    % get convarians matrix
+    C = phi' * phi;
+    
+    % get eigenvectors
+    [eigenVectors, ~ ] = eig(C);
+    
+    % calculate eigenfaces
+    eigenFaces = zeros(size(phi));
+    for k = 1:1:n
+        eigenFaces(:, k) = phi * eigenVectors(:, k);
+    end
+    
+    % TODO: här borde vi bara ta de bästa egenvektorerna, om vi har flera
+    % bilder på samma person
+    weights = eigenFaces' * phi;
+end
